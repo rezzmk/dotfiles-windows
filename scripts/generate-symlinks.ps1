9b8064339c4c905692c17be581bb37bcd1fa774a
unknown <marcos.caramalho@gmail.com>
2021-04-14 22:37:43 +0100

    Reformatting code for pure prettyness

--- scripts/generate-symlinks.ps1
+++ scripts/generate-symlinks.ps1
@@ -1,12 +1,11 @@
 # Yeah we need to run this as administrator to create a symbolic link :)
 #Requires -RunAsAdministrator
 
+$DotfilesCommonPath = "$(Split-Path (Split-Path $PSScriptRoot -Parent) -Parent)" + "\dotfiles-common" 
 
 # The reason we do this and not something like '../../dotfiles-common' is because I'll be calling this script
 # from deeper directories and in order to not make it break this is the best way I found. It's ugly af but so is Windows and Powershell
 Write-Output "Generating Symlinks, listing variables..."
-$DotfilesCommonPath = "$(Split-Path (Split-Path $PSScriptRoot -Parent) -Parent)" + "\dotfiles-common" 
-
 Write-Output "`tdotfiles-common path: $DotfilesCommonPath"
 
 # For jetbrains ideavim plugin configuration
