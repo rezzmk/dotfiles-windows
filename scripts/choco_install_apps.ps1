65d8686250956c22f10d015c5861a840a48ae791
unknown <marcos.caramalho@gmail.com>
2021-09-22 21:35:07 +0100

    Adds check for already installed choco apps

--- scripts/choco_install_apps.ps1
+++ scripts/choco_install_apps.ps1
@@ -1,11 +1,35 @@
+[string[]]$installedApplications = choco list --local-only --limit-output
 [string[]]$applicationsArray = Get-Content -Path choco-apps-to-install.txt
 
+for ($i = 0; $i -lt $installedApplications.Count; $i++) {
+	$installedApplications[$i] = $installedApplications[$i].Split("|")[0]
+}
+
 foreach ($app in $applicationsArray) {
-	Write-Host "Installing $app ..."
-	& choco install $app --yes --limit-output
+	if (-Not ($installedApplications -contains $app)) {
+		Write-Host "Installing $app ..." -ForegroundColor Yellow
+		& choco install $app --yes --limit-output
+	}
+	else {
+		Write-Host "$app is already installed!" -ForegroundColor Red
+	}
 }
 
 Write-Host "Installing applications with custom parameters..."
 
-choco install vim --yes --params '/NoDesktopShortcuts /NoDefaultVimrc'
-choco install git --yes --params '/GitAndUnixToolsOnPath /WindowsTerminal'
+# TODO: This is fugly, fix it
+if (-Not ($installedApplications -contains "git")) {
+	Write-Host "Installing vim..." -ForegroundColor Yellow
+	choco install vim --yes --params '/NoDesktopShortcuts /NoDefaultVimrc'
+}
+else {
+	Write-Host "vim is already installed!" -ForegroundColor Red
+}
+
+if (-Not ($installedApplications -contains "git")) {
+	Write-Host "Installing git..." -ForegroundColor Yellow
+	choco install git --yes --params '/GitAndUnixToolsOnPath /WindowsTerminal'
+}
+else {
+	Write-Host "git is already installed!" -ForegroundColor Red
+}
