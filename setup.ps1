cc6d3fb1a1a0d02959d0de6c3fdbbf59c6b09640
unknown <marcos.caramalho@gmail.com>
2021-09-22 16:34:07 +0100

    Updates setup.ps1, .gitignore (nerd-fonts)

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,8 @@
 #Requires -RunAsAdministrator
 
+$ScriptsPath = $PSScriptRoot + "\scripts"
+$ConfigurationsPath = $PSScriptRoot + "\configs"
+
 # Copy and load modules
 $modulesPath = "$home\Documents\WindowsPowerShell\Modules"
 
@@ -9,46 +12,55 @@ Copy-Item .\utilities.psm1 -Destination $modulesPath\utilities\utilities.psm1 | 
 
 Import-Module utilities
 
-$ScriptsPath = $PSScriptRoot + "\scripts"
-$ConfigurationsPath = $PSScriptRoot + "\configs"
-
-# 
-# Chocolatey
 #
+# Setup
+#
+
+# Chocolatey
 RunAction -m "(SETUP) Installing Chocolatey..." -a { 
 	Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
 } 
 
-#
 # Vim
-#
 RunAction -m  "(SETUP) Installing Vim..." -a {
 	choco install vim --yes --params '/NoDesktopShortcuts /NoDefaultVimrc'
 }
 
-#
 # Git - Installation
-#
 RunAction -m  "(SETUP) Installing Git..." -a {
 	choco install git --yes --params '/GitAndUnixToolsOnPath /WindowsTerminal'
 }
 
-#
 # Git - Configuration
-#
 RunAction -m "(SETUP) Configuring Git..." -a {
 	.("$ConfigurationsPath\git-config.ps1")
 }
 
-# Git - PS tooling
-#Install-Module posh-git -Force -Scope CurrentUser
-#Install-Module oh-my-posh -Force -Scope CurrentUser
-#Set-Prompt
-#Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
-#Add-Content $PROFILE "`nImport-Module posh-git`nImport-Module oh-my-posh`nSet-Theme Paradox"
-#
-#Write-Output "(SETUP) Configuring windows explorer (windows-configs.ps1)"
-#.("$ConfigurationsPath\windows-explorer-config.ps1")
+# Windows Terminal Plugins
+RunAction -m "(SETUP) Set-up Windows Terminal for Git (posh)" -a {
+	Install-Module posh-git -Force -Scope CurrentUser
+	Install-Module oh-my-posh -Force -Scope CurrentUser
+	Set-Prompt
+	Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
+	Add-Content $PROFILE "`nImport-Module posh-git`nImport-Module oh-my-posh`nSet-Theme Paradox"
+}
 
-#Write-Output "Calling generate-symlinks.ps1"
-#.("$ScriptsPath\generate-symlinks.ps1")
+# Nerd Fonts
+RunAction -m "(SETUP) Installing nerd-fonts..." -a {
+	git clone https://github.com/ryanoasis/nerd-fonts.git
+	Set-Location nerd-fonts
+	.\install.ps1
+	Set-Location ..\
+
+	Write-Host "MANUALLY SETUP FONT ON WINDOWS TERMINAL CONFIG, MesloLGM NF" -ForegroundColor Red
+}
+
+# Windows Explorer Settings
+RunAction -m "(SETUP) Configuring Windows Explorer settings..." -a {
+	.("$ConfigurationsPath\windows-explorer-config.ps1")
+}
+
+# Symlinks
+RunAction -m "(SETUP) Generating symlinks..." -a {
+	.("$ScriptsPath\generate-symlinks.ps1")
+}
